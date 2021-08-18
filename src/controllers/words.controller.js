a4d77658ed5453fbc610c3c4e404a66e94785a52
orangecn23 <orangecn23@gmail.com>
2021-08-18 00:45:36 -0500

    v1.2.0 adicion de endpoint 2 al proyecto

--- src/controllers/words.controller.js
+++ src/controllers/words.controller.js
@@ -1,3 +1,7 @@
 export const reverseSentencesDB = (req, res) => {
   res.status(200).json('From reverse sentences DB')
 }
+
+export const reverseSentencesDB3 = (req, res) => {
+  res.status(200).json('From reverse sentences DB 3')
+}
--- src/routes/words.routes.js
+++ src/routes/words.routes.js
@@ -1,11 +1,14 @@
 import { Router } from 'express'
 import {
-  reverseSentencesDB
+  reverseSentencesDB, reverseSentencesDB3
 } from '../controllers/words.controller.js'
 
 const router = Router()
 
 // Route to compare two sentences with DB
 router.get('/level-1', reverseSentencesDB)
+
+// Route to compare two sentences with DB
+router.get('/level-3', reverseSentencesDB3)
 
 export default router
